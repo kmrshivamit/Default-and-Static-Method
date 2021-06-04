89ebdb8b1ff45145041dc19253b15bf2469572da
M1064451 <M1064451@MT-PG02AL2E>
2021-06-04 17:25:04 +0530

    Added in new feature branch

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("In new feature branch");
 		// TODO Auto-generated method stub
  ChildClass childClass=new ChildClass();
  childClass.show();
